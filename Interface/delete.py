82b762650a14aae15bca7cb06de0678f29a19815
Wenyang Hu <hwy0821@yeah.net>
2025-07-17 16:22:51 +0800

    新增create和delete接口

---
+++ Interface/delete.py
@@ -0,0 +1,22 @@
+import requests
+
+token = "9591bca2739d476ea4ef77ce3df5908d"
+
+id = "7547db7284954afd980dae5a091d3487"
+
+# 接口地址，添加 id 作为查询参数
+url = f"http://123.60.179.95:48090/admin-api/cms/policy/delete?id={id}"  # 使用之前创建的 id
+
+
+# 请求头
+headers = {
+    "Authorization": f"Bearer {token}",
+    "Content-Type": "application/json"
+}
+
+# 发起 DELETE 请求（无需 json 数据体）
+resp = requests.delete(url, headers=headers)
+
+# 打印返回内容
+print("状态码:", resp.status_code)
+print("响应内容:", resp.text)
